406a448ac510eb88e01d96a2aa623ae07b336880
Alinahaseeb1 <aishahasib321@gmail.com>
2024-02-23 02:03:34 +0500

    Work Complete

---
+++ assignment3/lib/widgets/pageview.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class page_view extends StatefulWidget {
+  const page_view({super.key});
+
+  @override
+  State<page_view> createState() => _page_viewState();
+}
+
+class _page_viewState extends State<page_view> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 18, 117, 21),
+          centerTitle: false,
+          title: const Text(
+            "WhatsApp",
+            style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+          bottom: const TabBar(
+            tabs: [
+              Tab(child: Icon(Icons.camera_alt)),
+              Tab(
+                child: Text("CHATS"),
+              ),
+              Tab(
+                child: Text("STATUS"),
+              ),
+              Tab(
+                child: Text("CALLS"),
+              )
+            ],
+          ),
+          actions: [
+            const Icon(Icons.search),
+            const SizedBox(
+              width: 10,
+            ),
+            PopupMenuButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                itemBuilder: (context) => [
+                      const PopupMenuItem(value: "1", child: Text("new group")),
+                      const PopupMenuItem(value: "2", child: Text("settings")),
+                      const PopupMenuItem(value: "3", child: Text("Logout")),
+                    ]),
+            const SizedBox(
+              width: 10,
+            )
+          ],
+        ),
+        body: TabBarView(children: [
+          const Text("camera"),
+          ListView.builder(
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return const ListTile(
+                  title: Text("Zain"),
+                  subtitle: Text("hello Flutter!"),
+                  trailing: Text("8:25pm"),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage("url"),
+                  ),
+                );
+              }),
+           ListView.builder(
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return const ListTile(
+                  title: Text("Zain"),
+                  subtitle: Text("hello Flutter!"),
+                  trailing: Text("8:25pm"),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage("url"),
+                  ),
+                );
+              }),
+          ListView.builder(
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return const ListTile(
+                  title: Text("Zain"),
+                  subtitle: Text("hello Flutter!"),
+                  trailing: Text("8:25pm"),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage("url"),
+                  ),
+                );
+              }),
+        ]),
+      ),
+    );
+  }
+}
+
+myTile() {
+  return ListView.builder(
+      itemCount: 2,
+      itemBuilder: (context, index) {
+        return const ListTile(
+          title: Text("Zain"),
+          subtitle: Text("Hello Flutter!"),
+          trailing: Text("8:25pm"),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage("url"),
+          ),
+        );
+});
+}
+
